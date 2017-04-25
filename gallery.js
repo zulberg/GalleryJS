ce5d931cc07df0a9d3727fa206bd7887ec7aef13
Unknown <zulberg@mail.csuchico.edu>
2017-04-25 15:54:06 -0700

    Finished

--- gallery.js
+++ gallery.js
@@ -1,9 +1,14 @@
 var chico = ["Bidwell Park", "Honey Run Bridge", "Sierra Nevada Brewery", "Five Mile Recreation Area", "Diamond Alley"];
+var current= 0;
 
 function shuffleGallery()
 {
-  Math.floor(Math.random(chico) * 4);
-  for(var ind=0; ind<chico.size; ind++) {
-    print(ind);
+  do {
+    var indx = Math.floor(Math.random() * (chico.length));
+    current=chico[indx];
+    if (current != indx) {
+    console.log(current);
+    }
   }
+  while(current != chico[indx]);
 }
